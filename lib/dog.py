a5321f7e735d59297b90b6d79e360f849b2e217f
jameskimani001 <jameskim092t@gmail.com>
2024-11-29 11:13:26 +0300

    object initialization complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+if __name__ == "__main__":
+    fido = Dog("Fido")
+    print(fido.name)
+    print(fido.breed)
+
+    snoopy = Dog("Snoopy", "Beagle")
+    print(snoopy.name)
+    print(snoopy.breed)
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
+# lib/person.py
+
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+if __name__ == "__main__":
+    john = Person("John")
+    print(john.name)
+
